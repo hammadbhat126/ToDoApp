f4ba909517185de63f946e9bc1ab307b2ad7adb5
Hammad <hammad@rpi.ae>
2021-09-20 17:21:46 +0400

    UI ADDED

---
+++ app/src/main/java/com/kashsoft/todoapp/create_card.kt
@@ -0,0 +1,11 @@
+package com.kashsoft.todoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class create_card : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_card)
+    }
+}
---
+++ app/src/main/java/com/kashsoft/todoapp/update_card.kt
@@ -0,0 +1,11 @@
+package com.kashsoft.todoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class update_card : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_card)
+    }
+}
