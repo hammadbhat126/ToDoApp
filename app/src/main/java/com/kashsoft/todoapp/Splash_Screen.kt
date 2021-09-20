189010a1b22cad45522d200167c939690fc1467a
Hammad <hammad@rpi.ae>
2021-09-20 16:57:43 +0400

    splashscreen

---
+++ app/src/main/java/com/kashsoft/todoapp/Splash_Screen.kt
@@ -0,0 +1,11 @@
+package com.kashsoft.todoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
